3ba65f305fc00cb2d445e776db0ee79f0fe0fb6d
Oleg <demekhov@yandex.ru>
2022-08-05 16:34:47 +0300

    hello console

---
+++ ex001_hello console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex001_hello_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("123 Hello, World!");
+        }
+    }
+}
